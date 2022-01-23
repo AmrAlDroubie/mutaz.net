b8dbc3d39c6301aee2f1faf3cd543fc83ed25320
AmrAldroubie <amraldroubie@hotmail.com>
2022-01-23 20:16:09 +0200

    new version

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 // open list
 $(".program-list-title").click(function(){
-    $(this).siblings(".program-list-content").toggleClass("open").slideToggle(200);
+    $(this).siblings(".program-list-content").toggleClass("open").slideToggle(500);
     $(this).toggleClass("active")
 })
-
 
 // open program
 $(".pro-link").click(function(e){
@@ -11,9 +10,6 @@ $(".pro-link").click(function(e){
     $(this).siblings(".pro-info").slideToggle(200);
     $(this).parent(".pro-item").siblings().children(".pro-info").slideUp(200);
 })
-
-
-
 
 // Dark mode
 let modeBtn = document.querySelector("#mode-btn");
@@ -43,28 +39,8 @@ modeBtn.onclick = function(e){
             headDom.append(linkDarkAr);
         }
     }
-    
-    localStorage.setItem("mode",mode);
-}
-
-window.onload = function(){
-    if(localStorage.getItem("mode")=="light"){
-        if(document.querySelector("head link[href='css/dark_mode.css']")){
-            document.querySelector("head link[href='css/dark_mode.css']").remove();
-            document.querySelector("head link[href='css/dark_mode_rtl.css']").remove();
-            modeBtn.querySelector("i").className = "fas fa-moon"
-        }
-    }else{
-        headDom.append(linkDark);
-        if($("body").css("direction") == "rtl"){
-            headDom.append(linkDarkAr);
-        }
-        modeBtn.querySelector("i").className = "mutaz mutaz-sun"
-    }
 
 }
-
-
 
 // expand lists
 let expandState = true;
@@ -87,8 +63,6 @@ document.querySelector("#expand-lists").onclick = function(){
         $(".program-list-title").addClass("active")
     }
 }
-
-
 
 
 // search submit
